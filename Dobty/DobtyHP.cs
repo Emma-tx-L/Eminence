de2cecff7d596cfd9c4b97684f55dec15bbe3298
Emma Liu <gigitxl@yahoo.ca>
2020-01-10 19:15:59 -0800

    Survival mode, Light theme 90% implemented

--- Dobty/DobtyHP.cs
+++ Dobty/DobtyHP.cs
@@ -1,35 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DobtyHP : MonoBehaviour {
     public float maxBrightness = 3f;
     public float minBrightness = 0.5f;
 
-    Material material;
+    public int maxHP = 10;
+    public int currentHP;
 
-    // Use this for initialization
+    public int scoreIncrement = 1;
+    public int scoreDecrement = -2;
+
+    public ParticleSystem goodEffect;
+    public ParticleSystem badEffect;
+
+    private Slider HPSlider;
+    private float brightnessIncrement = 0.1f;
+    private float iframe = 0.5f;
+    private bool iframed;
+    private Material material;
+
     void Start () {
         material = GetComponentInChildren<Renderer>().material;
+        currentHP = maxHP;
+        iframed = false;
+        SetUpHPSlider();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    IEnumerator CycleBrightness(float initial, float target, float cycleTime)
+    private void SetUpHPSlider()
     {
-        //isCycling = true;
+        HPSlider = ReferenceManager.refManager.HPSlider;
+        HPSlider.maxValue = maxHP;
+        HPSlider.minValue = 0;
+        HPSlider.value = currentHP;
+    }
+
+    private void ChangeBrightness(bool positive)
+    {
+        float change = positive ? brightnessIncrement : -brightnessIncrement;
+        float newBrightness = Mathf.Clamp(material.GetFloat("_Brightness") + change, minBrightness, maxBrightness);
+        material.SetFloat("_Brightness", newBrightness);
+    }
+
+    public void DeductHP(int deduction)
+    {
+        if (!iframed)
+        {
+            currentHP = Mathf.Clamp(currentHP - deduction, 0, maxHP);
+            if (currentHP <= 0)
+            {
+                GameManager.gameManager.EndGame();
+            }
+            ChangeBrightness(false);
+            badEffect.Play();
+            //ReferenceManager.refManager.scoreManager.UpdatePoints(scoreDecrement);
+            HPSlider.value = currentHP;
+            StartCoroutine(MakeInvulnerable());
+        }
+    }
+
+    public void AddHP(int addition)
+    {
+        if (!iframed)
+        {
+            //currentHP = Mathf.Clamp(currentHP + addition, 0, maxHP);
+            ChangeBrightness(true);
+            goodEffect.Play();
+            ReferenceManager.refManager.scoreManager.UpdatePoints(scoreIncrement);
+            HPSlider.value = currentHP;
+            StartCoroutine(MakeInvulnerable());
+        }
+    }
+
+    public IEnumerator MakeInvulnerable()
+    {
+        iframed = true;
         float currentTime = 0;
-        while (currentTime < cycleTime)
+        while (currentTime < iframe)
         {
             currentTime += Time.deltaTime;
-            float t = currentTime / cycleTime;
-            float currentBrightness = Mathf.Lerp(initial, target, t);
-            material.SetFloat("_Brightness", currentBrightness);
             yield return null;
         }
-        //isCycling = false;
+        iframed = false;
     }
 }
---
+++ Portals/PortalActions.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalActions : MonoBehaviour {
+    public float medianInterval = 8f;
+    public int points = 2;
+    public int damage = 2;
+
+    private ParticleSystem goodSpark;
+    private ParticleSystem badSpark;
+
+    private float openingInterval;
+    private bool playerInRange;
+
+    private Animator anim;
+    private float timer;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+        goodSpark = GetComponent<PortalAnimations>().goodSpark;
+        badSpark = GetComponent<PortalAnimations>().badSpark;
+        ResetTimer();
+        SetNeutral();
+	}
+	
+	void Update () {
+        CycleActions();
+	}
+
+    private void CycleActions()
+    {
+        if (timer >= openingInterval)
+        {
+            int fate = Random.Range(0, 2);
+            if (fate == 0)
+            {
+                SetBad();
+            }
+            else
+            {
+                SetGood();
+            }
+            ResetTimer();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void ResetTimer()
+    {
+        timer = 0f;
+        openingInterval = Random.Range(medianInterval - 3, medianInterval + 3);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.GetComponent<DobtyHP>() != null)
+        {
+            bool isGood = goodSpark.isPlaying;
+            bool isBad = badSpark.isPlaying;
+
+            if (isGood && !isBad)
+            {
+                other.gameObject.GetComponent<DobtyHP>().AddHP(points);
+            }
+            else if (!isGood && isBad)
+            {
+                other.gameObject.GetComponent<DobtyHP>().DeductHP(damage);
+            }
+        }
+    }
+
+    public void SetNeutral()
+    {
+        anim.ResetTrigger("isBad");
+        anim.ResetTrigger("isGood");
+    }
+
+    public void SetBad()
+    {
+        anim.SetTrigger("isBad");
+    }
+
+    public void SetGood()
+    {
+        anim.SetTrigger("isGood");
+    }
+}
